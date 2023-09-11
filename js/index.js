190959a3e375c88a517ad5dd18c72a8bbb51e0ce
XuanYing0915 <xuanyinglife@gmail.com>
2023-09-12 04:07:52 +0800

    大致完成

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ new WOW().init();
 $(document).ready(function () {
 	setTimeout(function () {
 		$('.loading-wrapper').fadeOut(500);
-	}, 100);
+	}, 2000);
 });
 
 $(window).scroll(function () {
@@ -10,10 +10,5 @@ $(window).scroll(function () {
 		$('#headerNav').removeClass('display-none');
 	} else {
 		$('#headerNav').addClass('display-none');
-	}
-	// 當滾動到about時，about才開始跑動化
-	if ($(this).scrollTop() > 900) {
-		$('.about-title').addClass('about-title-animation');
-		$('.about-content').addClass('about-content-animation');
 	}
 });
